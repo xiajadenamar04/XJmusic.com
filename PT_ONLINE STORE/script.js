258441c403875ea36d8566fc30b5d6f0c9016bd5
xiajadenamar04 <106165764+xiajadenamar04@users.noreply.github.com>
2022-05-24 22:40:49 +0800

    Add files via upload

---
+++ PT_ONLINE STORE/script.js
@@ -0,0 +1,35 @@
+function bigImg(x) {
+x.style.height = "50%";
+x.style.width= "50%";
+}
+
+function normalImg(x) {
+x.style.height = "40%";
+x.style.width= "40%";
+}
+
+function mouseOUT1() {
+document.getElementById("demo1").style.color= "blue";
+}
+function mouseIN1() {
+document.getElementById("demo1").style.color= "red";}
+    
+function mouseOUT2() {
+document.getElementById("demo2").style.color= "blue";}
+function mouseIN2() {
+document.getElementById("demo2").style.color= "red";}
+
+function mouseOUT3() {
+document.getElementById("demo3").style.color= "blue";}
+function mouseIN3() {
+document.getElementById("demo3").style.color= "red";}
+
+function mouseOUT4() {
+document.getElementById("demo4").style.color= "blue";}
+function mouseIN4() {
+document.getElementById("demo4").style.color= "red";}
+
+function mouseOUT5() {
+document.getElementById("demo5").style.color= "blue";}
+function mouseIN5() {
+document.getElementById("demo5").style.color= "red";}
---
+++ PT_ONLINE STORE/script1.js
@@ -0,0 +1,30 @@
+function bigImg(x) {
+x.style.height = "25%";
+x.style.width= "25%";
+}
+
+function normalImg(x) {
+x.style.height = "15%";
+x.style.width= "15%";
+}
+
+function mouseOUT1() {
+document.getElementById("demo1").style.color= "blue";
+}
+function mouseIN1() {
+document.getElementById("demo1").style.color= "red";}
+    
+function mouseOUT2() {
+document.getElementById("demo2").style.color= "blue";}
+function mouseIN2() {
+document.getElementById("demo2").style.color= "red";}
+
+function mouseOUT3() {
+document.getElementById("demo3").style.color= "blue";}
+function mouseIN3() {
+document.getElementById("demo3").style.color= "red";}
+
+function mouseOUT4() {
+document.getElementById("demo4").style.color= "blue";}
+function mouseIN4() {
+document.getElementById("demo4").style.color= "red";}
